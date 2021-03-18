85bc5aa5d86da54e7863bc64438be5492085fce5
Alexander Yukal <yukal.alexander@mapviser.com>
2021-03-18 10:27:00 +0200

    refactoring: lucky number

--- test.js
+++ test.js
@@ -1,32 +1,34 @@
 function findLucky(items) {
-  const length = items.length;
-  const matches = {};
-  let max = 0;
+  const hashmap = {};
+  let luckyMatches = 0;
+  let luckyNumber = 0;
 
-  for (let n = 0; n < length; n++) {
-    const num = items[n];
-
-    if (!matches.hasOwnProperty(num)) {
-      matches[num] = 1;
+  items.forEach((number) => {
+    if (!hashmap.hasOwnProperty(number)) {
+      hashmap[number] = 1;
     } else {
-      matches[num] += 1;
+      hashmap[number] += 1;
 
-      if (matches[num] > max) {
-        max = matches[num];
+      // The ">" strategy will save only first matched lucky number
+      // The ">=" strategy will identify the last matched lucky number
+      if (hashmap[number] >= luckyMatches) {
+        luckyMatches = hashmap[number];
+        luckyNumber = number;
       }
     }
-  }
+  });
 
-  return max > 1 ? max : -1;
+  return luckyNumber !== 0 ? luckyNumber : false;
 }
 
 const cases = [
   [1, 2, 2, 3, 4],
   [2, 3, 4, 5],
   [9, 9, 9, 9, 9, 9, 9, 9, 9, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 1],
+  [2, -1, -1, 5, -2, -2],
 ];
 
-cases.map((items) => {
+cases.forEach((items) => {
   const luckyNumber = findLucky(items);
   console.log(luckyNumber);
 });
